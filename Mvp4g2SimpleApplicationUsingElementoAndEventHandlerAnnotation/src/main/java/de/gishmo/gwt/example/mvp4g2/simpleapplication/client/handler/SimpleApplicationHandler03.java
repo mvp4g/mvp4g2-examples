8c82603060994d3634b860c0df09fb01eeceaf8f
Frank Hossfeld <frank.hossfeld@googlemail.com>
2018-03-17 00:11:44 +0100

    Improvements

--- Mvp4g2SimpleApplicationUsingElementoAndEventHandlerAnnotation/src/main/java/de/gishmo/gwt/example/mvp4g2/simpleapplication/client/handler/SimpleApplicationHandler03.java
+++ Mvp4g2SimpleApplicationUsingElementoAndEventHandlerAnnotation/src/main/java/de/gishmo/gwt/example/mvp4g2/simpleapplication/client/handler/SimpleApplicationHandler03.java
@@ -14,29 +14,29 @@ import elemental2.dom.Element;
  * for any ecvnt we handle, we will use a alert to present a info message.
  */
 @Handler
-public class SimpleApplicationHandler02
+public class SimpleApplicationHandler03
   extends AbstractHandler<Mvp4g2SimpleApplicationUsingElementoAndEventHandlerAnnotationEventBus> {
 
   @EventHandler
   public void onSetNavigation(Element element) {
-    DomGlobal.window.alert("SimpleApplicationHandler02: -> Navigation is set!");
+    DomGlobal.window.alert("SimpleApplicationHandler03: -> Navigation is set!");
   }
 
   @EventHandler
   public void onGotoDetail(long id) {
-    DomGlobal.window.alert("SimpleApplicationHandler02: -> fire event 'gotoDetail' with key: >>" + id + "<<");
+    DomGlobal.window.alert("SimpleApplicationHandler03: -> fire event 'gotoDetail' with key: >>" + id + "<<");
   }
 
   @EventHandler
   public void onGotoList(String searchName,
                          String searchOrt) {
-    DomGlobal.window.alert("SimpleApplicationHandler02: -> fire event 'gotoList' with searchName: >>" + searchName + "<< and searchName >>" + searchName + "<<");
+    DomGlobal.window.alert("SimpleApplicationHandler03: -> fire event 'gotoList' with searchName: >>" + searchName + "<< and searchName >>" + searchName + "<<");
   }
 
   @EventHandler
   public void onGotoSearch(String searchName,
                            String searchOrt) {
-    DomGlobal.window.alert("SimpleApplicationHandler02: -> fire event 'gotoSearch' with searchName: >>" + searchName + "<< and searchName >>" + searchName + "<<");
+    DomGlobal.window.alert("SimpleApplicationHandler03: -> fire event 'gotoSearch' with searchName: >>" + searchName + "<< and searchName >>" + searchName + "<<");
   }
 
 }
